d1938503277481bf641a1635a03e21878f1dfe7a
Felipe Baltor <fbaltor@gmail.com>
2021-06-05 22:15:46 -0300

    simple ls implementation in C; must check how the dirent.h header works

---
+++ myls.c
@@ -0,0 +1,31 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void err_sys(const char *message, char *fd) {
+  printf(message, fd);
+  exit(0);
+}
+void err_quit(const char *message) {
+  printf("%s", message);
+  exit(0);
+}
+
+int main(int argc, char *argv[]) {
+  DIR *dp;
+  struct dirent *dirp;
+
+  if (argc != 2) {
+    char *message = "usage: ls directory_name";
+    err_quit(message);
+  }
+
+  if ((dp = opendir(argv[1])) == NULL)
+    err_sys("can’t open %s", argv[1]);
+
+  while ((dirp = readdir(dp)) != NULL)
+    printf("%s\n", dirp->d_name);
+  closedir(dp);
+
+  exit(0);
+}
