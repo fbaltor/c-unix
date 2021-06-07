e880c74609e4c5f28e68efb8b1a361421a9e61df
Felipe Baltor <fbaltor@gmail.com>
2021-06-07 00:20:32 -0300

    minor organization

--- eof.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int a = EOF;
-  printf("%d\n", a);
-
-  return 0;
-}
--- tese.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#define MAXLINE 1000 // Maximum line lenght.
-
-int main() {
-  getline(1,1,1);
-  return 0;
-}
