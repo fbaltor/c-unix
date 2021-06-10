c6248d45859e9d51b8f18bba786db5c663dd1ebb
Felipe Baltor <fbaltor@gmail.com>
2021-06-10 02:44:54 -0300

    WIP maxline2

---
+++ the_c_programming_language/ch1/maxline2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum input line length */
+
+// getline alt
+int getline_alt(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print the longest input */
+int main() {
+  int len;
+  int max;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max = 0;
+  while ((len = getline_alt(line, MAXLINE)) > 0)
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  if (max > 0) /* there was a line */
+    printf("%s", longest);
+  return 0;
+}
+
+// getline alternative implementation
+int getline_alt(char s[], int lim) {
+  int c, i = 0;
+
+  while ((c = getchar()) != EOF && c != '\n') {
+    if (i < lim - 1)
+      s[i] = c;
+    if (i = lim - 1)
+      s[i] = '\n';
+    ++i;
+  }
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[]) {
+  int i;
+  i = 0;
+  while ((to[i] = from[i]) != '\0')
+    ++i;
+}
